88e132d115fc183b28c4fccc5f9cdc22ab1798a8
bhanuprakashpodugu <43947029+bhanuprakashpodugu@users.noreply.github.com>
2019-06-17 16:56:12 +0530

    2 more files

---
+++ BasicParser.java
@@ -0,0 +1,53 @@
+package com.ocjp.practise.virtusa.code.test;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class BasicParser {
+	
+	public static void main(String argv[]) {
+
+	    try {
+
+		File fXmlFile = new File("C:\\CPQWorkArea\\CodeSamples\\src\\input.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+				
+		doc.getDocumentElement().normalize();
+
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+				
+		NodeList nList = doc.getElementsByTagName("staff");
+				
+		System.out.println("----------------------------");
+
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+
+			Node nNode = nList.item(temp);
+					
+			System.out.println("\nCurrent Element :" + nNode.getNodeName());
+					
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element eElement = (Element) nNode;
+
+				System.out.println("Element Name : " + eElement.getNodeName());
+				
+
+			}
+		}
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    }
+	  }
+
+
+}
